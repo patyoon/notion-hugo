fa9abf006ef9108bd4d7c2766e9a9c100c9ad538
Patrick Yeo Ho Yoon <patyoon@users.noreply.github.com>
2024-06-01 16:03:14 -0700

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-DoIt-dba1de995fa0409b9a19b807e05c81c6?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
