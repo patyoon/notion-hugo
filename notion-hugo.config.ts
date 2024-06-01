6f521cfd2cd58a110d742e647dea00c7c953eba3
Patrick Yeo Ho Yoon <patyoon@users.noreply.github.com>
2024-06-01 16:47:01 -0700

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -9,20 +9,12 @@ const userConfig: UserConfig = {
             //     page_id: '<page_id>',
             //     target_folder: 'path/relative/to/content/folder'
             // }
-            {
-                page_id: '45eb121158b9489480ec000fd25c812b',
-                target_folder: '.'
-            }
         ],
         databases: [
             // {
             //     database_id: '<database_id>',
             //     target_folder: 'path/relative/to/content/folder'
             // }
-            {
-                database_id: 'b7b1816c05ec464391c8c111fa242985',
-                target_folder: '.'
-            }
         ],
     }
 }
