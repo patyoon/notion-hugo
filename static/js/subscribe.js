57c7ba63f0b204bb606545ddf0d61828c8807941
Patrick Yoon <me@patyoon.com>
2026-02-07 12:24:59 -0800

    update subscribe form

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -1,36 +1,27 @@
-var form = document.getElementById("subscribe-form");
+const form = document.getElementById("subscribe-form");
 if (form !== null) {
-    form.addEventListener("submit", formSubmit);
+  form.addEventListener("submit", formSubmit);
 }
 
 function formSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append(
-        'email',
-        document.querySelector('input[name="email"]').value
-    );
-    grecaptcha.ready(function() {
-        // do request for recaptcha token
-        // response is promise with passed token
-        grecaptcha.execute('6LdG0n0hAAAAAGI740Voq8YqSp0LEDzNepZIHoSd', {action: 'contact'}).then(function(token) {
-            // add token to form
-            formData.append('g-recaptcha-response', token);
-            fetch("https://getform.io/f/51c5ce85-9f44-49af-805c-c98ebb34fe4f",
-                  {
-                      method: "POST",
-                      body: formData,
-                  })
-                .then( response => {
-                    function redirect() {
-                        window.location.href = "/page/subscribed/";
-                    }
-                    if (response['ok']) {
-                        console.log(response);
-                        redirect();
-                    }
-                })
-                .catch(error => console.log(error));
-        });
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  // Submit to Netlify Forms
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = "/page/subscribed/";
+      } else {
+        throw new Error("Form submission failed");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("There was an error submitting the form. Please try again.");
     });
-};
+}
